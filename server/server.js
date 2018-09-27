09e1919a91e9c4db94c9c06f8b9adaeb80dc3268
Yuliyul <ykhazran@gmail.com>
2018-09-27 18:46:13 +0300

    Add post TODOS rout

---
+++ server/server.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+
+var app = express();
+app.use(bodyParser.json());
+app.post('/todos', (req,res)=>{
+	var ntodo = new Todo({
+		text : req.body.text,
+		completed:req.body.completed
+	});
+	ntodo.save().then((doc)=>{
+		res.send(doc);
+	},
+	(e)=>{
+		res.send(e);
+	});
+});
+app.listen(3000, ()=>{console.log('Started on port 3000')});
+
+
+
+
+
+// var newTodo = new Todo({
+// 	text:'Cook maffins'
+// });
+
+
+// var newTodo = new Todo({
+// 	text:'Learn node',
+// 	completed:false,
+// 	completedAt:null
+// });
+// newTodo.save().then((doc)=>{
+// 	console.log('Saved ', doc);
+// },
+// 	(e)=>{
+// 		console.log('cannot save');
+// 	});
