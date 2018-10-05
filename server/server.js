e1bdec15bdbd645b50d49869c69746aca7787d9c
Yuliyul <ykhazran@gmail.com>
2018-10-05 23:10:29 +0300

    add delete route

--- server/server.js
+++ server/server.js
@@ -41,6 +41,20 @@ app.get('/todos/:id', (req, res) => {
 	})
 
 });
+app.delete('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	if(!(ObjectId.isValid(id)))
+		res.status(404).send();
+	Todo.findByIdAndDelete(id).then((todos)=>{
+		if(!todos)
+			return res.status(404).send();
+		res.send({todos});
+	},
+	(e)=>{
+		res.status(400).send();
+	})
+
+});
 app.listen(port, ()=>{console.log(`Started on port ${port}`)});
 
 module.exports = {app};
